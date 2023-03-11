47488935e4e74f295c9a027c2b524cc6ecd7bb4f
Erica Brisigotti <115556238+ericabrisigotti@users.noreply.github.com>
2023-03-11 22:42:30 +0100

    Add files via upload

---
+++ function_for_ex0.py
@@ -0,0 +1,10 @@
+# function to be run in the context of "Exercise 0" from 01ex_Fundamentals.ipynb
+
+# firstly, I define the function
+def is_there_breaking_italy_today(today):
+	if today:
+		print("Bentornati su Breaking Italy!")
+	else:
+		print("[alpaca]")
+
+# that I will later run from my usual Jupyterlab file
